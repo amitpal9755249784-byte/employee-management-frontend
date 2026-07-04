7a3975887cdf13e5634c1226a0abffbbee079256
Amit Baghel <amitpal9755249784@gmail.com>
2026-07-04 11:05:44 +0530

    Add profile and change password page

---
+++ src/pages/ChangePassword.jsx
@@ -0,0 +1,20 @@
+function ChangePassword() {
+  return (
+    <div className="container mt-4">
+      <div className="card shadow">
+        <div className="card-header bg-primary text-white">
+          <h3>Change Password</h3>
+        </div>
+
+        <div className="card-body">
+          <h5>Coming Soon...</h5>
+          <p>
+            Change Password feature will be added in the next step.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChangePassword;
